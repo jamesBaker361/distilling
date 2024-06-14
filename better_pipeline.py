54cc9ceafda2cec65da89fad35dadaa5a2cf1643
James Baker <jlbaker361@gmail.com>
2024-06-14 14:32:40 -0400

    less printing

--- better_pipeline.py
+++ better_pipeline.py
@@ -3,4 +3,5 @@ from diffusers import StableDiffusionPipeline
 
 class BetterPipeline(StableDiffusionPipeline):
     def run_safety_checker(self, image, device, dtype):
+        #print("safe???")
         return image, None
